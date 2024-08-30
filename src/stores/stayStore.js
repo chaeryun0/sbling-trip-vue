09f205a9938f5d770a99efd32e98934592016f77
김채령 <kimchaeryung@gimchaelyeong-ui-iMac.local>
2024-08-30 14:55:04 +0900

    🔨 [ADD] 숙소 데이터형식 변환 및 타입별 데이터 가져오기

--- src/stores/stayStore.js
+++ src/stores/stayStore.js
@@ -1,33 +1,69 @@
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
-import http from '@utils/http.js'
+import { ref } from 'vue';
+import { defineStore } from 'pinia';
+import http from '@utils/http';
 
 export const useStayStore = defineStore('stayStore', () => {
-  const stays = ref([])
-  const currentPage = ref(0)
+  const stays = ref([]);
+  const staysByType = ref({});
+  const currentPage = ref(0);
+
+  const formatLocation = (address) => {
+    const [province, city] = address.split(' ', 2);
+    const filteredProvince = province.slice(0, 2);
+    return `${filteredProvince} ∙ ${city}`;
+  };
+
+  const removeParentheses = (str) =>
+    str?.replace(/\((.*?)\)/g, '').trim();
+
+  const formatStays = (data) =>
+    data.map((stay) => ({
+      ...stay,
+      stayName: removeParentheses(stay.stayName),
+      originalAddress: stay.address,
+      formattedAddress: formatLocation(stay.address),
+    })
+    );
 
   const getStays = async () => {
     try {
-      const response = await http.get(`/stay/list?cursor=${currentPage.value}`)
+      const { data } = await http.get(`/stay/list?cursor=${currentPage.value}`);
       if (currentPage.value === 0) {
-        stays.value = response.data.result
+        stays.value = formatStays(data.result);
       } else {
-        stays.value = [...stays.value, ...response.data.result]
+        stays.value = [...stays.value, ...formatStays(data.result)];
       }
     } catch (error) {
-      console.error('숙소 데이터를 가져오는 중 에러 발생:', error)
+      console.error('숙소 데이터를 가져오는 중 에러 발생:', error);
     }
-  }
+  };
+
+  const getStaysByType = async (type) => {
+    try {
+      const { data } = await http.get(`/stay/list/recommend/review-rank?stayType=${type}`);
+      staysByType.value[type] = formatStays(data.result);
+    } catch (error) {
+      console.error('숙소 타입 별 데이터를 가져오는 중 에러 발생:', error);
+    }
+  };
 
   const loadMoreStays = () => {
-    currentPage.value += 1
-    getStays()
-  }
+    currentPage.value += 1;
+    getStays();
+  };
+
+  const handleStayTypeChange = async (type) => {
+    if (!staysByType.value[type]) {
+      await getStaysByType(type);
+    }
+  };
 
   return {
     stays,
+    staysByType,
     currentPage,
     getStays,
     loadMoreStays,
-  }
-})
+    handleStayTypeChange,
+  };
+});
